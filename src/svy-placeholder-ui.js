7889def8ead21b77aa4aea921ab99be45714c140
pruhsert <pruhsert@servoy.com>
2020-12-20 18:19:46 +0100

    v0.1.1

--- src/svy-placeholder-ui.js
+++ src/svy-placeholder-ui.js
@@ -15,57 +15,57 @@ export default class SvyPlaceholderUi extends Plugin {
      * @inheritDoc
      */
 	init() {
-	const editor = this.editor;
-  const t = editor.t;
-  const placeholderNames = [ 'date', 'first name', 'surname' ];
+        const editor = this.editor;
+        const t = editor.t;
+        const placeholderNames = [ 'date', 'first name', 'surname' ];
 
-  // The "placeholder" dropdown must be registered among the UI components of the editor
-  // to be displayed in the toolbar.
-  editor.ui.componentFactory.add( 'placeholder', locale => {
-      const dropdownView = createDropdown( locale );
+        // The "placeholder" dropdown must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add( 'placeholder', locale => {
+            const dropdownView = createDropdown( locale );
 
-      // Populate the list in the dropdown with items.
-      addListToDropdown( dropdownView, getDropdownItemsDefinitions( placeholderNames ) );
+            // Populate the list in the dropdown with items.
+            addListToDropdown( dropdownView, this.getDropdownItemsDefinitions( placeholderNames ) );
 
-      dropdownView.buttonView.set( {
-          // The t() function helps localize the editor. All strings enclosed in t() can be
-          // translated and change when the language of the editor changes.
-          label: t( 'Placeholder' ),
-          tooltip: true,
-          withText: true
-      } );
+            dropdownView.buttonView.set( {
+                // The t() function helps localize the editor. All strings enclosed in t() can be
+                // translated and change when the language of the editor changes.
+                label: t( 'Placeholder' ),
+                tooltip: true,
+                withText: true
+            } );
 
-      // Disable the placeholder button when the command is disabled.
-      const command = editor.commands.get( 'placeholder' );
-      dropdownView.bind( 'isEnabled' ).to( command );
+            // Disable the placeholder button when the command is disabled.
+            const command = editor.commands.get( 'placeholder' );
+            dropdownView.bind( 'isEnabled' ).to( command );
 
-      // Execute the command when the dropdown item is clicked (executed).
-      this.listenTo( dropdownView, 'execute', evt => {
-          editor.execute( 'placeholder', { value: evt.source.commandParam } );
-          editor.editing.view.focus();
-      } );
+            // Execute the command when the dropdown item is clicked (executed).
+            this.listenTo( dropdownView, 'execute', evt => {
+                editor.execute( 'placeholder', { value: evt.source.commandParam } );
+                editor.editing.view.focus();
+            } );
 
-      return dropdownView;
+            return dropdownView;
 		});
 	}
 	
-	function getDropdownItemsDefinitions( placeholderNames ) {
-    const itemDefinitions = new Collection();
+	getDropdownItemsDefinitions( placeholderNames ) {
+        const itemDefinitions = new Collection();
 
-    for ( const name of placeholderNames ) {
-        const definition = {
-            type: 'button',
-            model: new Model( {
-                commandParam: name,
-                label: name,
-                withText: true
-            } )
-        };
+        for ( const name of placeholderNames ) {
+            const definition = {
+                type: 'button',
+                model: new Model( {
+                    commandParam: name,
+                    label: name,
+                    withText: true
+                } )
+            };
 
-        // Add the item definition to the collection.
-        itemDefinitions.add( definition );
-    }
+            // Add the item definition to the collection.
+            itemDefinitions.add( definition );
+        }
 
-    return itemDefinitions;
+        return itemDefinitions;
 	}
 }
